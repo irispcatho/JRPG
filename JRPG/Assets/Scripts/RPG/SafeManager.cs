130c3f9274c0ca06707abd78dcca62e0fb446038
irispecquet <iris.pecquet@lacatholille.fr>
2022-04-01 23:30:13 +0200

    coffre (ne pas y toucher pr l'instant)

---
+++ JRPG/Assets/Scripts/RPG/SafeManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeManager : MonoBehaviour
+{
+    public Upgrade[] upgrade;
+    public bool isInRange;
+    public GameObject[] safeSprite;
+
+    private void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            safeSprite[0].SetActive(false);
+            safeSprite[1].SetActive(true);
+            TriggerSafe();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    private void TriggerSafe()
+    {
+        int rnd = Random.Range(3, 5);
+        print(rnd);
+        for (int i = 0; i < rnd; i++)
+        {
+            upgrade[i].cardAffected.power += upgrade[i].attackUpgrade;
+        }
+    }
+}
